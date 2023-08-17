c56b874a940462e89122f070cc1d72819b705dbc
Bob Belderbos <bobbelderbos@gmail.com>
2023-08-17 18:35:38 +0200

    fix command line script

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from translate import translate_text
 
 def main():
     while True:
-        destination = input("Enter target language (q to exit, p to print history): ")
+        destination = input("Enter target language (q to exit): ")
         if destination == "q":
             print("Exiting...")
             break
-        elif destination == "p":
-            print("Translation History:")
-            translations = retrieve_translations()
-            for t in translations:
-                print(t)
-            continue
 
         while True:
-            text = input("Enter text to translate (q to quit): ")
+            text = input("Enter text to translate (q to quit, p to print history): ")
             if text == "q":
                 break
+
+            if text == "p":
+                print("Translation History:")
+                translations = retrieve_translations(destination)
+                for row in translations:
+                    print(f"{row.text:<50} | {row.translated_text}")
+                print()
+                continue
+
             translated_text = translate_text(text, destination)
             print(translated_text)
             store_translation(text, destination, translated_text)
